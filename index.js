d41de240dd0e6c675ef7972797a4352f61eb5dcb
awongh <akira@awongh.com>
2021-03-13 17:46:33 +0800

    add express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const whenIncomingRequest = (request, response) => {
+  console.log('request came in');
+  response.send('yay');
+};
+
+app.get('/', whenIncomingRequest);
+
+app.listen(PORT);
