5ab7dc2dcc743b713e3604115a3631e5717803d4
awongh <akira@awongh.com>
2021-03-15 23:24:21 +0800

    add subtract test

--- test/calculator.js
+++ test/calculator.js
@@ -1,12 +1,17 @@
 import { expect } from 'chai';
 
-import { add } from '../calculator.js';
+import { add, subtract } from '../calculator.js';
 
 describe('Calculator', function () {
   describe('Basic Operations', function () {
     it('Adds two numbers', function () {
       const result = add(2, 2);
       expect(result).to.equal(4);
+    });
+
+    it('Subtracts two numbers', function () {
+      const result = subtract(2, 2);
+      expect(result).to.equal(0);
     });
   });
 });
